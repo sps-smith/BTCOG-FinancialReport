96e1e1d2e7bf807088b98b6493d973ba19973164
Phil Smith <phil.smith@volarisgroup.com>
2018-02-05 19:29:36 -0500

    EOD 02/05/2018

--- dist/FinancialReports/Financial.js
+++ dist/FinancialReports/Financial.js
@@ -1,9 +1,12 @@
 
+
 var store = new Vuex.Store({
     state:{
       _years: [2014,2015,2016,2017, 2018, 2019, 2020],
       _months: ['','January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      _selectedPeriod: ""
+      _selectedPeriod: "",
+      _expenses:[],
+      _revenue:[]
     },
     getters: {
         getMonths: function(state){
@@ -19,20 +22,105 @@ var store = new Vuex.Store({
     mutations:{
         setPeriod: function(state, payload){
             state._selectedPeriod = payload;
+        },
+        setExpenses: function(state, payload){
+            state._expenses = payload;
+        },
+        setRevenue: function (state, payload) {
+          state._revenue = payload;
         }
     },
     actions: {
         GetFinanceData: function(context, payload){
+          var wrkary = [];
           context.commit("setPeriod", context.state._months[payload.parm] + " " + payload.parm1);
-          axios.get(_spPageContextInfo.webServerRelativeUrl + "/_api/web/lists/getbytitle('Expense Transactions')/items?$filter=Year eq " + payload.parm1 + " and Month eq " + payload.parm, {
+          axios.get(_spPageContextInfo.webServerRelativeUrl + "/_api/web/lists/getbytitle('Expense Transactions')/items?$select=TransactionType/Title,TransactionDate,Amount&$filter=TransactionDate ge datetime'" + getFinanceStartDate(payload.parm, payload.parm1) + "' and TransactionDate le datetime'" + getFinanceEndDate(payload.parm, payload.parm1) + "'&$expand=TransactionType/Id", {
             headers: { "accept": "application/json;odata=verbose" }
+          })
+          .then(function(results){
+              results.data.d.results.forEach(function(element){
+                  var fnd = wrkary.filter(function(item){
+                      return item.Expense == element.TransactionType.Title;
+                  });
+                  if (fnd.length == 0)
+                  {
+                      var dt = {
+                          Expense: element.TransactionType.Title,
+                          Period:[0,0,0,0,0]
+                      }
+
+                      dt.Period[getPeriodDay(element.TransactionDate)-1] = element.Amount;
+                      wrkary.push(dt)
+                  }
+                  else
+                  {
+                      $.each(wrkary, function(idx, item){
+                          if (item.Expense == fnd[0].Expense)
+                          {
+                              item.Period[getPeriodDay(element.TransactionDate) - 1] += element.Amount;
+                              return false;
+                          }
+                      })
+                  }
+              });
+              context.commit("setExpenses", wrkary);
+          })
+          .then(function(data){
+              axios.get(_spPageContextInfo.webServerRelativeUrl + "/_api/web/lists/getbytitle('Revenue Transactions')/items?$select=TransactionType/Title,TransactionDate,Amount&$filter=TransactionDate ge datetime'" + getFinanceStartDate(payload.parm, payload.parm1) + "' and TransactionDate le datetime'" + getFinanceEndDate(payload.parm, payload.parm1) + "'&$expand=TransactionType/Id", {
+                headers: { "accept": "application/json;odata=verbose" }
+              })
+              .then(function(results){
+                  wrkary = [];
+                  results.data.d.results.forEach(function (element) {
+                      var fnd = wrkary.filter(function (item) {
+                        return item.Revenue == element.TransactionType.Title;
+                      });
+                      if (fnd.length == 0) {
+                        var dt = {
+                          Revenue: element.TransactionType.Title,
+                          Period: [0, 0, 0, 0, 0]
+                        }
+
+                        dt.Period[getPeriodDay(element.TransactionDate) - 1] = element.Amount;
+                        wrkary.push(dt)
+                      }
+                      else {
+                        $.each(wrkary, function (idx, item) {
+                          if (item.Revenue == fnd[0].Revenue) {
+                            item.Period[getPeriodDay(element.TransactionDate) - 1] += element.Amount;
+                            return false;
+                          }
+                        })
+                      }
+                  });
+                  context.commit("setRevenue", wrkary);
+              })
           })
 
         }
     }
 })
 
+function getPeriodDay(dte){
+    var dy = new Date(dte).getDate()
+    if (dy <= 7)
+      return 1;
+    else
+    {
+        var nm = Math.round(dy / 7);
+        var rm = dy % 7;
+        if (rm > 0)
+          nm += 1;
+        return nm
+    }
+}
 
-function getFinanceDate(mn, yr){
-    var fdate = "01/" + mm + "/" + yr;
+function getFinanceStartDate(mn, yr){
+    var fdate = mn + "/01/" + yr;
+  return new Date(fdate).toISOString().substring(0, 10) + "T00:00:00";
+}
+
+function getFinanceEndDate(mn, yr) {
+    var edate = Date.getDaysInMonth(yr, mn)
+  return new Date(mn + "/" + edate + "/" + yr).toISOString().substring(0, 10) + "T00:00:00";
 }
